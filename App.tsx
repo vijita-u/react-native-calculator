95432adc6e64eb193659e15125f97a38986614c3
Vijita Udayabhanu <vijita@density.exchange>
2024-05-08 12:58:44 +0530

    Features and styling added

--- App.tsx
+++ App.tsx
@@ -1,118 +1,166 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, Pressable} from 'react-native';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#000',
+  },
+  heading: {
+    color: '#fff',
+    fontSize: 30,
+    textAlign: 'right',
+    marginRight: 20,
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: '#000',
+    width: 100,
+    height: 100,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  sum: {
+    backgroundColor: '#87CEEB',
+    width: 100,
+    height: 100,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  subcontainer: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  calculator: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  number: {
+    color: '#fff',
+    fontSize: 24,
+  },
+  operand: {
+    color: '#FFA500',
+    fontSize: 24,
+  },
+  calculate: {
+    color: '#000',
+    fontSize: 24,
+  },
+  display: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+  },
+});
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const App = () => {
+  const [display, setDisplay] = useState<string>('0');
+  const [operand, setOperand] = useState<number | null>(null);
+  const [operator, setOperator] = useState<string | null>(null);
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  const pressButton = (value: string) => {
+    if (display === '0') {
+      setDisplay(value);
+    } else {
+      setDisplay(display + value); // Concatenating strings
+    }
+  };
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+  const pressOperator = (operation: string) => {
+    setOperand(parseFloat(display));
+    setOperator(operation);
+    setDisplay('0');
+  };
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const calculate = () => {
+    const num = parseFloat(display);
+    switch (operator) {
+      case '+':
+        setDisplay((operand + num).toString());
+        break;
+      case '-':
+        setDisplay((operand - num).toString());
+        break;
+      case '*':
+        setDisplay((operand * num).toString());
+        break;
+      case '/':
+        setDisplay((operand / num).toString());
+        break;
+    }
+    setOperand(null);
+    setOperator(null);
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <View style={styles.container}>
+      <View style={styles.display}>
+        <Text style={styles.heading}>{display}</Text>
+      </View>
+      <View style={styles.calculator}>
+        <View style={styles.subcontainer}>
+          <Pressable style={styles.button} onPress={() => pressButton('1')}>
+            <Text style={styles.number}>1</Text>
+          </Pressable>
+          <Pressable style={styles.button} onPress={() => pressButton('2')}>
+            <Text style={styles.number}>2</Text>
+          </Pressable>
+          <Pressable style={styles.button} onPress={() => pressButton('3')}>
+            <Text style={styles.number}>3</Text>
+          </Pressable>
+          <Pressable style={styles.button} onPress={() => pressButton('4')}>
+            <Text style={styles.number}>4</Text>
+          </Pressable>
+          <Pressable style={styles.button} onPress={() => pressButton('5')}>
+            <Text style={styles.number}>5</Text>
+          </Pressable>
+          <Pressable style={styles.button} onPress={() => pressButton('6')}>
+            <Text style={styles.number}>6</Text>
+          </Pressable>
+          <Pressable style={styles.button} onPress={() => pressButton('7')}>
+            <Text style={styles.number}>7</Text>
+          </Pressable>
+          <Pressable style={styles.button} onPress={() => pressButton('8')}>
+            <Text style={styles.number}>8</Text>
+          </Pressable>
+          <Pressable style={styles.button} onPress={() => pressButton('9')}>
+            <Text style={styles.number}>9</Text>
+          </Pressable>
+          <Pressable style={styles.button} onPress={() => pressButton('0')}>
+            <Text style={styles.number}>0</Text>
+          </Pressable>
+          <Pressable style={styles.button} onPress={() => pressOperator('+')}>
+            <Text style={styles.operand}>+</Text>
+          </Pressable>
+          <Pressable style={styles.button} onPress={() => pressOperator('-')}>
+            <Text style={styles.operand}>-</Text>
+          </Pressable>
+          <Pressable style={styles.button} onPress={() => pressOperator('/')}>
+            <Text style={styles.operand}>/</Text>
+          </Pressable>
+          <Pressable style={styles.button} onPress={() => pressOperator('*')}>
+            <Text style={styles.operand}>*</Text>
+          </Pressable>
+          <Pressable style={styles.button} onPress={calculate}>
+            <Text style={styles.operand}>=</Text>
+          </Pressable>
+          <Pressable style={styles.sum} onPress={() => setDisplay('0')}>
+            <Text style={styles.calculate}>C</Text>
+          </Pressable>
         </View>
-      </ScrollView>
-    </SafeAreaView>
+      </View>
+    </View>
   );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+};
 
 export default App;
